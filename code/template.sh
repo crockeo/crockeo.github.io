46c6886f07cc3c4f5903d5615ea18464446112d1
Cerek Hillen <chillen@lyft.com>
2020-12-25 01:18:34 -0500

    subsume header and footer

--- code/template.sh
+++ code/template.sh
@@ -2,23 +2,6 @@
 
 set -e
 
-md_file="$1"
-output_file="$2"
-if [ -z "$md_file" ]; then
-    echo "ERR: must provide path to markdown file"
-    exit 1
-fi
-if [ -z "$output_file" ]; then
-    echo "ERR: must provide output file"
-    exit 1
-fi
-
-if [ ! -f "$md_file" ]; then
-    echo "ERR: markdown file must exist"
-    exit 1
-fi
-
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [ ! -f "$DIR/templater" ]; then
     original_dir=$(pwd)
@@ -26,8 +9,4 @@ if [ ! -f "$DIR/templater" ]; then
     make
     cd "$original_dir"
 fi
-
-
-cat "$DIR/header.html" > "$output_file"
-$DIR/templater "$md_path" >> "$output_file"
-cat "$DIR/footer.html" >> "$output_file"
+$DIR/templater $@
--- code/templater.c
+++ code/templater.c
@@ -3,49 +3,107 @@
 #include <stdlib.h>
 #include <string.h>
 
-int convert(FILE *in_markdown, FILE *out_html) {
-    // TODO: ensure we set errno when we return a non-zero result
-    errno = ENOSYS;
+// pipe, self-descriptively, pipes the contents from an input file to an output file.
+//
+// Returns:
+//   If the operation succeeds, pipe returns 0.
+//   If the operation fails, pipe returns -1 and errno is set.
+int pipe(FILE *in, FILE *out) {
+  size_t read_size = 16;
+  char buf[read_size];
+
+  size_t bytes_read;
+  size_t bytes_written;
+
+  while ((bytes_read = fread(buf, 1, read_size, in))) {
+    bytes_written = 0;
+    size_t new_bytes_written;
+    while (bytes_written < bytes_read &&
+           (new_bytes_written = fwrite(&buf[bytes_written], 1,
+                                       bytes_read - bytes_written, out))) {
+      bytes_written += new_bytes_written;
+    }
+
+    if (errno != 0) {
+      return -1;
+    }
+  }
+
+  if (errno != 0) {
     return -1;
+  }
+
+  return 0;
 }
 
-// read in markdown file
-//
-// convert to html file
+// pipe_from wraps pipe to also open the file in question for reading. See the
+// documentation for pipe for information on return values.
+int pipe_from(const char *src_file, FILE *out) {
+  FILE *in = fopen(src_file, "r");
+  if (in == NULL) {
+    return -1;
+  }
+  int ret = pipe(in, out);
+  fclose(in);
+  return ret;
+}
+
+int convert(FILE *in_markdown, FILE *out_html) {
+  // TODO: ensure we set errno when we return a non-zero result
+  errno = ENOSYS;
+  return -1;
+}
 
 int main(int argc, char **argv) {
-    if (argc < 2 || argc > 3) {
-        printf("correct usage:\n");
-        printf("  %s <path/to/markdown.md> [path/to/output.html]\n", argv[0]);
-        printf("\n");
-        printf("  if the output path is not provided, then this program\n");
-        printf("  print the results straight to stdout.\n");
-        return 1;
-    }
+  if (argc < 2 || argc > 5) {
+    printf("correct usage:\n");
+    printf("  %s <path/to/markdown.md> [path/to/header.html] [path/to/footer.html] [path/to/output.html]\n", argv[0]);
+    printf("\n");
+    printf("  if the output path is not provided, then this program\n");
+    printf("  print the results straight to stdout.\n");
+    return 1;
+  }
 
-    FILE *in_markdown = fopen(argv[1], "r");
-    if (in_markdown == NULL) {
-        printf("ERR: failed to open markdown file '%s'.\n", argv[1]);
-        printf("     %s\n", strerror(errno));;
-        return 1;
-    }
+  FILE *in_markdown = fopen(argv[1], "r");
+  if (in_markdown == NULL) {
+    printf("ERR: failed to open markdown file '%s'.\n", argv[1]);
+    printf("     %s\n", strerror(errno));
+    return 1;
+  }
 
-    FILE *out_html;
-    if (argc == 3) {
-        out_html = fopen(argv[2], "w");
-        if (out_html == NULL) {
-            printf("ERR: failed to open out_html file '%s'.\n", argv[2]);
-            printf("     %s\n", strerror(errno));
-            return 1;
-        }
-    } else {
-        out_html = stdout;
+  FILE *out_html;
+  if (argc >= 5) {
+    out_html = fopen(argv[4], "w");
+    if (out_html == NULL) {
+      printf("ERR: failed to open out_html file '%s'.\n", argv[4]);
+      printf("     %s\n", strerror(errno));
+      return 1;
     }
+  } else {
+    out_html = stdout;
+  }
 
-    if (convert(in_markdown, out_html)) {
-        printf("ERR: failed to map markdown to html.\n");
-        printf("     %s\n", strerror(errno));
+  if (argc >= 3) {
+    const char *header_location = argv[2];
+    if (pipe_from(header_location, out_html)) {
+      printf("ERR: failed to pipe header '%s'.\n", argv[2]);
+      printf("     %s\n", strerror(errno));
+      return 1;
     }
+  }
 
-    return 0;
+  if (convert(in_markdown, out_html)) {
+    printf("ERR: failed to map markdown to html.\n");
+    printf("     %s\n", strerror(errno));
+  }
+
+  if (argc >= 4) {
+    const char *footer_location = argv[3];
+    if (pipe_from(footer_location, out_html)) {
+      printf("ERR: failed to pipe footer '%s'.\n", argv[3]);
+      printf("     %s\n", strerror(errno));
+    }
+  }
+
+  return 0;
 }
